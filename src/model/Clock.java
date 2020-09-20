5ae1895c5a14af2a5dddd132d245700f668987eb
larkala <44373792+larkala@users.noreply.github.com>
2020-09-20 17:46:31 +0200

    Is it committing
    Javadoc to Clock

--- src/model/Clock.java
+++ src/model/Clock.java
@@ -1,5 +1,11 @@
 package model;
 
+/**
+ * This class contains the Clock that will later be used as a part of the chess clock.
+ * 
+ * @author larkala
+ * @version 2020-09-20
+ */
 public class Clock {
 
 }
