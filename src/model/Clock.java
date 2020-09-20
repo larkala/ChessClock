3e5632a2eec28885f9de70d5fcfd80a72b2404a7
larkala <44373792+larkala@users.noreply.github.com>
2020-09-20 18:56:13 +0200

    Rly not sure if this works
    let's try

--- src/model/Clock.java
+++ src/model/Clock.java
@@ -1,39 +1,60 @@
 package model;
 
 /**
- * This class contains the Clock that will later be used as a part of the chess clock.
+ * This class contains the Clock that will later be used as a part of the chess
+ * clock.
  * 
  * @author larkala
  * @version 2020-09-20
  */
 
 public class Clock {
-	
+
 	private int ticks, hh, mm, ss;
 	private boolean running;
-	
-	/**
-	 * Makes the clock "tick" once. Adds a second to the total seconds and updates the hours,
-	 * minutes and seconds relative to total ticks.
-	 */
-	private void tick() {
-		
+
+	public Clock() {
+		ticks = hh = mm = ss = 0;
+		running = false;
 	}
-	
+
+	/**
+	 * Makes the clock "tick" once. Adds a second to the total seconds and updates
+	 * the hours, minutes and seconds relative to total ticks.
+	 * 
+	 * @throws InterruptedException 
+	 */
+	private void tick() throws InterruptedException {
+		Thread.sleep(1000);
+		ticks++;
+	}
+
 	/**
 	 * Starts the clock.
+	 * @throws InterruptedException 
 	 */
-	public void start() {
-		
+	public void start() throws InterruptedException {
+		if (!isRunning()) {
+			running = true;
+
+			while (running) {
+				tick();
+
+				hh = ticks / 3600;
+				mm = (ticks % 3600) / 60;
+				ss = ticks % 60;
+			}
+		}
 	}
-	
+
 	/**
 	 * Pauses the clock.
 	 */
 	public void pause() {
-		
+		if (isRunning())
+			running = false;
 	}
-	
+
 	/**
 	 * Resets the clock.
 	 */
@@ -42,7 +63,7 @@ public class Clock {
 			pause();
 		ticks = 0;
 	}
-	
+
 	/**
 	 * Tells if clock is running.
 	 * 
@@ -51,14 +72,14 @@ public class Clock {
 	public boolean isRunning() {
 		return running;
 	}
-	
+
 	/**
 	 * Returns the time passed as an integer array.
 	 * 
 	 * @return list of hours, minutes & seconds of this clock
 	 */
 	public int[] getTime() {
-		int [] time = {hh, mm, ss};
+		int[] time = { hh, mm, ss };
 		return time;
 	}
 }
